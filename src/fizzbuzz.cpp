a7c0c5d57af1da5bf6285fdc7f4654ddacb9be13
kawasakitoshiya <kawasakitoshiya@gmail.com>
2013-10-08 21:46:09 +0900

    FizzBuzz::say says FizzBuzz if given value is multiple of 15

--- src/fizzbuzz.cpp
+++ src/fizzbuzz.cpp
@@ -6,8 +6,10 @@ FizzBuzz::FizzBuzz(){
 
 std::string FizzBuzz::say(int num){
 
+  if(num%15 == 0) return "FizzBuzz";
   if(num%3 == 0) return "Fizz";
   if(num%5 == 0) return "Buzz";
+
 
   std::ostringstream stream;
   stream << num;
