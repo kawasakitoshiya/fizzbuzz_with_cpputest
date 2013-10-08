9c6e452b11d13c06b1e5b1e191181dae78a46eaa
kawasakitoshiya <kawasakitoshiya@gmail.com>
2013-10-08 21:31:16 +0900

    add test say2

--- test/fizzbuzz_test.cpp
+++ test/fizzbuzz_test.cpp
@@ -14,7 +14,13 @@ TEST_GROUP(FizzBuzz)
     delete fizzBuzz;
   }
 };
+
 TEST(FizzBuzz, TestSay1)
 {
   STRCMP_EQUAL("1", fizzBuzz->say(1).c_str()); 
+}
+
+TEST(FizzBuzz, TestSay2)
+{
+  STRCMP_EQUAL("2", fizzBuzz->say(2).c_str()); 
 }
